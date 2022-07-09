b0c91f668d227d87bec92f28e1c4383115fc52c1
André Welle <inf21177@lehre.dhbw-stuttgart.de>
2022-07-09 17:07:31 +0200

    feat: draw camps and runners

--- src/game/components/Cell.cpp
+++ src/game/components/Cell.cpp
@@ -6,16 +6,28 @@
 
 Cell::Cell(const sf::Vector2f &dim)
     : Component{ dim }
-{}
+{
+    setPieceType(PieceColor::COLOR_RED, PieceType::TYPE_CAMP);
+    setPieceType(PieceColor::COLOR_GREEN,PieceType::TYPE_CAMP);
+    setPieceType(PieceColor::COLOR_RED,PieceType::TYPE_RUNNER);
+}
 
 void
 Cell::setPieceType(PieceColor color, PieceType newType) {
-    m_occupiers[color] = newType;
+    if(newType == PieceType::TYPE_RUNNER){
+        m_runner[color] = newType;
+    }
+    else if(newType == PieceType::TYPE_CAMP) {
+        m_camps[color] = newType;
+    }
+    else{
+        m_runner.erase(color);
+    }
 }
 
 PieceType
 Cell::getPieceType(PieceColor color) {
-    return m_occupiers[color];
+    return m_camps[color];
 }
 
 void
@@ -24,5 +36,38 @@ Cell::draw(sf::RenderTarget &target, sf::RenderStates states) const {
     cellRect.setPosition(m_position);
     target.draw(cellRect);
 
-    //TODO: draw the occupiers
+    if(m_camps.size() == 1){
+        auto it = m_camps.begin();
+        cellRect.setFillColor(it->first==PieceColor::COLOR_GREEN ? sf::Color::Green : sf::Color::Red );
+        target.draw(cellRect);
+    }
+    else if(m_camps.size() == 2) {
+        sf::VertexArray campOne(sf::Triangles, 4);
+        campOne[0].position = sf::Vector2f(m_position);
+        campOne[1].position = sf::Vector2f(m_position.x + m_dimension.x, m_position.y);
+        campOne[2].position = sf::Vector2f(m_position.x + m_dimension.x, m_position.y + m_dimension.y);
+        campOne[0].color = sf::Color::Red;
+        campOne[1].color = sf::Color::Red;
+        campOne[2].color = sf::Color::Red;
+        target.draw(campOne);
+
+        sf::VertexArray campTwo(sf::Triangles, 3);
+        campTwo[0].position = sf::Vector2f(m_position);
+        campTwo[1].position = sf::Vector2f(m_position.x, m_position.y + m_dimension.y);
+        campTwo[2].position = sf::Vector2f(m_position.x + m_dimension.x, m_position.y + m_dimension.y);
+        campTwo[0].color = sf::Color::Green;
+        campTwo[1].color = sf::Color::Green;
+        campTwo[2].color = sf::Color::Green;
+        target.draw(campTwo);
+    }
+
+    if(!m_runner.empty()){
+        auto it = m_runner.begin();
+        sf::CircleShape runnerCircle(5,30);
+        runnerCircle.setOutlineThickness(2);
+        runnerCircle.setOutlineColor(sf::Color::Black);
+        runnerCircle.setPosition(m_position.x + m_dimension.x/2 - runnerCircle.getRadius(), m_position.y + m_dimension.y/2 - runnerCircle.getRadius());
+        runnerCircle.setFillColor(it->first==PieceColor::COLOR_GREEN ? sf::Color::Green : sf::Color::Red );
+        target.draw(runnerCircle);
+    }
 }
--- src/game/components/Cell.h
+++ src/game/components/Cell.h
@@ -9,9 +9,11 @@
 #include "Component.h"
 
 class Cell : public Component {
-    std::map<PieceColor, PieceType> m_occupiers;
+    std::map<PieceColor, PieceType> m_camps;
+    std::map<PieceColor, PieceType> m_runner;
+    sf::Color m_color = {};
 public:
-    Cell(const sf::Vector2f &dim);
+    explicit Cell(const sf::Vector2f &dim);
 
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 
